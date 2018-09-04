dd90f10d8bd81e6034a5bb9aa1ff3dce16f24336
Kolitha <s.kolitha@gmail.com>
2018-09-04 17:39:54 +1000

    Initial commit

---
+++ BethanyMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanyMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            //ViewBag.Message = "Welcome to Bethany’s Pie Shop";
+            return View();
+        }
+    }
+}
